c53d2e9734f78771befe577d54e041883d567dde
Teu Nome <teu-email@exemplo.com>
2026-05-09 19:05:57 +0000

    Increased body size limit to support Base64 images

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use('/imagens', express.static(path.join(__dirname, 'imagens')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
