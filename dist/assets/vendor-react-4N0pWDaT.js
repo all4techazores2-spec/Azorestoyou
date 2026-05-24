c8cb3f8d7883113375144ed268e9eba9c0756afd
Teu Nome <teu-email@exemplo.com>
2026-05-24 09:45:04 +0000

    Deploy fresh production build of dist folder

--- dist/assets/vendor-react-4N0pWDaT.js
+++ dist/assets/vendor-react-4N0pWDaT.js
@@ -1,4 +1,4 @@
-import{r as O}from"./vendor-icons-CAs2vd10.js";var y={exports:{}},n={};/**
+import{r as O}from"./vendor-icons-Cso0k7BN.js";var y={exports:{}},n={};/**
  * @license React
  * react-dom.production.js
  *
