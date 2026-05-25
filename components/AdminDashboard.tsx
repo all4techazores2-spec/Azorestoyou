2a1f806c1712c01f1293f8f39fb39435adfac262
Teu Nome <teu-email@exemplo.com>
2026-05-25 14:39:06 +0000

    Update and Deploy

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -13,7 +13,7 @@ import {
   Wrench, Zap, Hammer, Droplets, Paintbrush, HardHat, PencilRuler, 
   ThermometerSnowflake, DraftingCompass, Settings, ShoppingCart, 
   MessageSquare, Dog, Building2, Dumbbell, CarFront, Briefcase, Laptop, Pipette, Calendar, Database,
-  CheckCircle, AlertTriangle, ChevronLeft, ChevronRight
+  CheckCircle, AlertTriangle, ChevronLeft, ChevronRight, Wine
 } from 'lucide-react';
 
 import * as constants from '../constants';
